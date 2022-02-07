41de03f9580c33ab186049fda2eb726698c61d32
douggmc <douglas.mcintyre@gmail.com>
2022-02-07 15:10:52 -0500

    initial

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from urllib import response
+
 import requests
 import json
 import os
@@ -11,8 +13,15 @@ base_endpoint = os.getenv('BASE_ENDPOINT')
 oauth_base_endpoint = os.getenv('OAUTH_BASE_ENDPOINT')
 api_vendor = os.getenv('API_VENDOR')
 company_id = os.getenv('COMPANY_ID')
+api_refresh_token = os.getenv('API_REFRESH_TOKEN')
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+redirect_uri = os.getenv('REDIRECT_URI')
 
 header_token = {'Authorization': 'Bearer ' + access_token}
+
+g_response = ''
+g_ur = ''
 
 
 def c_outputer(jsonr):
@@ -27,24 +36,30 @@ def f_outputer(jsonr):
 
 
 def get_new_token():
-    l_ur = oauth_base_endpoint + api_refresh_token
-    print('x')
+    l_ur = oauth_base_endpoint + api_refresh_token + '&client_id=' + client_id + '&client_secret=' + client_secret + '&refresh_token=' + refresh_token + '&redirect_uri=' + redirect_uri
+    l_response = requests.post(l_ur, headers=header_token)
+    if l_response.status_code == 200:
+        os.environ['REFRESH_TOKEN'] = refresh_token
+        os.environ['ACCESS_TOKEN'] = access_token
+
+
+def get_vendor():
+    g_ur = base_endpoint + api_vendor + company_id
+    g_response = requests.get(ur, headers=header_token)
+    return response.status_code
 
 
 # Call Procore Get Vendor APIs
 def main():
-    ur = base_endpoint + api_vendor + company_id
-    response = requests.get(ur, headers=header_token)
-    if response.status_code == 401:
+    if get_vendor() == 401:
         get_new_token()
     else:
-        print(response.status_code)
+        print(get_vendor())
+        # Output to file.
+        f_outputer(response)
 
-    # Output to file.
-    f_outputer(response)
-
-    # Output to the console.
-    c_outputer(response)
+        # Output to the console.
+        c_outputer(response)
 
 
 if __name__ == "__main__":
