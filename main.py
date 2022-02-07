63f92165776b86e9756670c66f54721e6d7582dd
douggmc <douglas.mcintyre@gmail.com>
2022-02-07 14:10:01 -0500

    initial

--- main.py
+++ main.py
@@ -5,41 +5,47 @@ from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
-access_token = os.getenv("ACCESS_TOKEN")
-refresh_token = os.getenv("REFRESH_TOKEN")
-api_endpoint = os.getenv("API_ENDPOINT")
-company_id = os.getenv("COMPANY_ID")
+access_token = os.getenv('ACCESS_TOKEN')
+refresh_token = os.getenv('REFRESH_TOKEN')
+base_endpoint = os.getenv('BASE_ENDPOINT')
+oauth_base_endpoint = os.getenv('OAUTH_BASE_ENDPOINT')
+api_vendor = os.getenv('API_VENDOR')
+company_id = os.getenv('COMPANY_ID')
 
 header_token = {'Authorization': 'Bearer ' + access_token}
 
 
-def c_outputter(jsonr):
+def c_outputer(jsonr):
     outtxt = (json.dumps(jsonr.json(), indent=2))
     print(outtxt)
 
 
-def f_outputter(jsonr):
+def f_outputer(jsonr):
     file = open("vendor.json", "w")
     file.write(jsonr.text)
     file.close()
 
 
+def get_new_token():
+    l_ur = oauth_base_endpoint + api_refresh_token
+    print('x')
+
+
 # Call Procore Get Vendor APIs
 def main():
-    ur = api_endpoint + company_id
+    ur = base_endpoint + api_vendor + company_id
     response = requests.get(ur, headers=header_token)
     if response.status_code == 401:
-        ':except'
+        get_new_token()
     else:
         print(response.status_code)
 
     # Output to file.
-    f_outputter(response)
+    f_outputer(response)
 
     # Output to the console.
-    c_outputter(response)
+    c_outputer(response)
 
 
 if __name__ == "__main__":
     main()
-
